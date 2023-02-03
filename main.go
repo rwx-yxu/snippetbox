2a6f9072458ae9cce62729a012337ac20c9590cc
Yongle Xu <yonglexu08@gmail.com>
2023-02-03 19:02:16 +0000

    create header error response

--- main.go
+++ main.go
@@ -20,6 +20,18 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPOST {
+		// Use the Header().Set() method to add an 'Allow: POST' header to the
+		// response header map. The first parameter is the header name, and
+		// the second parameter is the header value.
+		w.Write.Set("Allow", htttp.MethodPOST)
+		// Use the http.Error() function to send a 405 status code and "Method Not
+		// Allowed" string as the response body.
+		// Usually rare to use w.Write() and w.WriteHeader() methods directly
+		// More common to send http.ResponseWriter
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 func main() {
