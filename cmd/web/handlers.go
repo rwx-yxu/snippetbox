887174da38d158e4850047bdb0ce975f5d87a945
Yongle Xu <yonglexu08@gmail.com>
2023-02-05 00:27:12 +0000

    template rendering

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -20,33 +20,11 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
-	}
 
-	/*
-		// Initialize a slice containing the paths to the two files. It's important
-		// to note that the file containing our base template must be the *first*
-		// file in the slice.
-		files := []string{
-			"./ui/html/base.tmpl",
-			"./ui/html/partials/nav.tmpl",
-			"./ui/html/pages/home.tmpl",
-		}
-		// Use the template.ParseFiles() function to read the files and store the
-		// templates in a template set. Notice that we can pass the slice of file
-		// paths as a variadic parameter?
-		ts, err := template.ParseFiles(files...)
-		if err != nil {
-			app.serverError(w, err)
-			return
-		}
-		// Use the ExecuteTemplate() method to write the content of the "base"
-		// template as the response body.
-		err = ts.ExecuteTemplate(w, "base", nil)
-		if err != nil {
-			app.serverError(w, err)
-		}*/
+	// Use the new render helper.
+	app.render(w, http.StatusOK, "home.tmpl", &templateData{
+		Snippets: snippets,
+	})
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -67,8 +45,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	// Write the snippet data as a plain-text HTTP response body.
-	fmt.Fprintf(w, "%+v", snippet)
+
+	// Use the new render helper.
+	app.render(w, http.StatusOK, "view.tmpl", &templateData{
+		Snippet: snippet,
+	})
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
