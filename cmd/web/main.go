69e63077a965ccc744a3928c75c46de91e47e234
Yongle Xu <yonglexu08@gmail.com>
2023-02-05 22:07:09 +0000

    user model scaffolding

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,6 +24,7 @@ type application struct {
 	errorLog       *log.Logger
 	infoLog        *log.Logger
 	snippets       *models.SnippetModel
+	users          *models.UserModel
 	templateCache  map[string]*template.Template
 	formDecoder    *form.Decoder
 	sessionManager *scs.SessionManager
@@ -90,6 +91,7 @@ func main() {
 		errorLog:       errorLog,
 		infoLog:        infoLog,
 		snippets:       &models.SnippetModel{DB: db},
+		users:          &models.UserModel{DB: db},
 		templateCache:  tc,
 		formDecoder:    fd,
 		sessionManager: sm,
