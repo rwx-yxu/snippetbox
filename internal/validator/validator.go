ec2ddc2d1f7247bdbfc52667665f054237eb2849
Yongle Xu <yonglexu08@gmail.com>
2023-02-07 21:17:28 +0000

    user login and logout logic supported

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -6,10 +6,11 @@ import (
 	"unicode/utf8"
 )
 
-// Define a new Validator type which contains a map of validation errors for our
-// form fields.
+// Add a new NonFieldErrors []string field to the struct, which we will use to
+// hold any validation errors which are not related to a specific form field.
 type Validator struct {
-	FieldErrors map[string]string
+	NonFieldErrors []string
+	FieldErrors    map[string]string
 }
 
 // Use the regexp.MustCompile() function to parse a regular expression pattern
@@ -18,11 +19,6 @@ type Validator struct {
 // this pattern once at startup and storing the compiled *regexp.Regexp in a
 // variable is more performant than re-parsing the pattern each time we need it.
 var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
-
-// Valid() returns true if the FieldErrors map doesn't contain any entries.
-func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
-}
 
 // AddFieldError() adds an error message to the FieldErrors map (so long as no
 // entry already exists for the given key).
@@ -74,4 +70,16 @@ func MinChars(value string, n int) bool {
 // expression pattern.
 func Matches(value string, rx *regexp.Regexp) bool {
 	return rx.MatchString(value)
+}
+
+// Update the Valid() method to also check that the NonFieldErrors slice is
+// empty.
+func (v *Validator) Valid() bool {
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+// Create an AddNonFieldError() helper for adding error messages to the new
+// NonFieldErrors slice.
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
